11fa29563a9e029a32b06af80ac12e57b65947a2
atrukhanovich <atrukhanovich@solvd.com>
2024-01-30 17:46:12 +0300

    open action window implementation

--- src/main/java/com/solvd/carinaideaplugin/GenerateIsPresentActionHandlerImpl.java
+++ src/main/java/com/solvd/carinaideaplugin/GenerateIsPresentActionHandlerImpl.java
@@ -1,16 +1,28 @@
 package com.solvd.carinaideaplugin;
 
 import com.intellij.codeInsight.CodeInsightActionHandler;
+import com.intellij.codeInsight.generation.PsiElementClassMember;
+import com.intellij.ide.util.MemberChooser;
+import com.intellij.java.JavaBundle;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.JavaCodeStyleSettings;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtil;
 import com.solvd.carinaideaplugin.filter.GenerateFilter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+
+import static com.intellij.codeInsight.daemon.impl.quickfix.AnonymousTargetClassPreselectionUtil.getPreselection;
+import static org.jetbrains.java.generate.GenerateToStringActionHandlerImpl.buildMembersToShow;
 
 public class GenerateIsPresentActionHandlerImpl implements GenerateIsPresentActionHandler, CodeInsightActionHandler {
     private static final Logger LOG = Logger.getInstance(GenerateIsPresentActionHandlerImpl.class);
@@ -33,7 +45,47 @@ public class GenerateIsPresentActionHandlerImpl implements GenerateIsPresentActi
             LOG.debug("Current project " + project.getName());
         }
 
+        final PsiElementClassMember<?>[] dialogMembers = buildMembersToShow(clazz);
+
+        final MemberChooserHeaderPanel header = new MemberChooserHeaderPanel(clazz);
+        LOG.debug("Displaying member chooser dialog");
+
+        final MemberChooser<PsiElementClassMember<?>> chooser =
+                new MemberChooser<>(dialogMembers, true, true, project, PsiUtil.isLanguageLevel5OrHigher(clazz), header) {
+                    @Override
+                    protected @NotNull String getHelpId() {
+                        return "editing.altInsert.tostring";
+                    }
+
+                    @Override
+                    protected boolean isInsertOverrideAnnotationSelected() {
+                        return JavaCodeStyleSettings.getInstance(clazz.getContainingFile()).INSERT_OVERRIDE_ANNOTATION;
+                    }
+                };
+        //noinspection DialogTitleCapitalization
+        chooser.setTitle("generate isElementPresent()");
+
+        chooser.setCopyJavadocVisible(false);
+        chooser.selectElements(getPreselection(clazz, dialogMembers));
+        header.setChooser(chooser);
+
+        LOG.debug("Displaying member chooser dialog");
+
+        if (ApplicationManager.getApplication().isUnitTestMode()) {
+            chooser.close(DialogWrapper.OK_EXIT_CODE);
+        }
+        else {
+            chooser.show();
+        }
+
     }
+
+    private static PsiElementClassMember<?>[] getPreselection(@NotNull PsiClass clazz, PsiElementClassMember<?>[] dialogMembers) {
+        return Arrays.stream(dialogMembers)
+                .filter(member -> member.getElement().getContainingClass() == clazz)
+                .toArray(PsiElementClassMember[]::new);
+    }
+
 
 
 
@@ -52,9 +104,9 @@ public class GenerateIsPresentActionHandlerImpl implements GenerateIsPresentActi
         }
 
         //exclude interfaces, non-java classes etc
-        for (GenerateFilter filter : GenerateFilter.EP_NAME.getExtensionList()) {
-            if (!filter.canGenerateToString(clazz)) return null;
-        }
+//        for (GenerateFilter filter : GenerateFilter.EP_NAME.getExtensionList()) {
+//            if (!filter.canGenerateToString(clazz)) return null;
+//        }
         return clazz;
     }
 }
