176a0053fda9fe2265127d1a4e243bc0f4211618
atrukhanovich <atrukhanovich@solvd.com>
2024-01-30 19:57:16 +0300

    fix MemberChooserHeaderPanel

--- src/main/java/com/solvd/carinaideaplugin/MemberChooserHeaderPanel.java
+++ src/main/java/com/solvd/carinaideaplugin/MemberChooserHeaderPanel.java
@@ -3,41 +3,11 @@ package com.solvd.carinaideaplugin;
 import com.intellij.codeInsight.generation.PsiElementClassMember;
 import com.intellij.ide.util.MemberChooser;
 import com.intellij.java.JavaBundle;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.options.Configurable;
-import com.intellij.openapi.options.ConfigurationException;
-import com.intellij.openapi.options.ShowSettingsUtil;
-import com.intellij.openapi.options.TabbedConfigurable;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.ComboBox;
-import com.intellij.openapi.ui.ComponentValidator;
-import com.intellij.openapi.ui.ValidationInfo;
-import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.ui.JBColor;
-import com.intellij.ui.SimpleListCellRenderer;
-import com.intellij.util.concurrency.AppExecutorUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.java.generate.GenerateToStringConfigurable;
-import org.jetbrains.java.generate.template.TemplateResource;
-import org.jetbrains.java.generate.template.TemplatesManager;
-import org.jetbrains.java.generate.template.toString.ToStringTemplatesManager;
-import org.jetbrains.java.generate.view.TemplatesPanel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.jetbrains.java.generate.GenerateToStringActionHandlerImpl.updateDialog;
 
 public class MemberChooserHeaderPanel extends JPanel {
 
