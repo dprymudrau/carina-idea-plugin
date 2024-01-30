5154dd80669704b9d9d94fa2fc9905f6d547b8a7
atrukhanovich <atrukhanovich@solvd.com>
2024-01-30 17:18:52 +0300

    add isDumbAware() (prevent working during indexing)

--- src/main/java/com/solvd/carinaideaplugin/GenerateIsPresentPopUpDialogAction.java
+++ src/main/java/com/solvd/carinaideaplugin/GenerateIsPresentPopUpDialogAction.java
@@ -4,6 +4,11 @@ import com.intellij.codeInsight.generation.actions.BaseGenerateAction;
 import com.intellij.openapi.project.DumbAware;
 
 public class GenerateIsPresentPopUpDialogAction extends BaseGenerateAction implements DumbAware {
+
+    @Override
+    public boolean isDumbAware() {
+        return false;
+    }
     public GenerateIsPresentPopUpDialogAction() {
         super(new GenerateIsPresentActionHandlerImpl());
     }
