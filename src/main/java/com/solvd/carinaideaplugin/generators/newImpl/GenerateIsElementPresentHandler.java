b0a7937380bf6f2f0fb72415561b4154fdfc720f
atrukhanovich <atrukhanovich@solvd.com>
2024-01-31 16:39:23 +0300

    connect to writeIsPresent()

--- src/main/java/com/solvd/carinaideaplugin/generators/newImpl/GenerateIsElementPresentHandler.java
+++ src/main/java/com/solvd/carinaideaplugin/generators/newImpl/GenerateIsElementPresentHandler.java
@@ -1,20 +1,27 @@
 package com.solvd.carinaideaplugin.generators.newImpl;
 
-import com.intellij.codeInsight.generation.ClassMember;
-import com.intellij.codeInsight.generation.GetterTemplatesManager;
-import com.intellij.codeInsight.generation.PsiElementClassMember;
+import com.intellij.codeInsight.generation.*;
 import com.intellij.codeInsight.hint.HintManager;
+import com.intellij.codeInsight.template.Template;
+import com.intellij.codeInsight.template.TemplateEditingAdapter;
+import com.intellij.codeInsight.template.TemplateManager;
 import com.intellij.ide.util.MemberChooser;
 import com.intellij.java.JavaBundle;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.ScrollType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.indexing.DumbModeAccessType;
 import com.solvd.carinaideaplugin.generators.ispresent.GenerateIsPresentActionHandlerImpl;
+import com.solvd.carinaideaplugin.utils.WebElementGrUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.java.generate.GenerateToStringContext;
@@ -24,7 +31,11 @@ import org.jetbrains.java.generate.config.Config;
 import org.jetbrains.java.generate.exception.GenerateCodeException;
 
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static com.solvd.carinaideaplugin.utils.WebElementGrUtil.getAvailableFields;
 
@@ -52,7 +63,7 @@ public class GenerateIsElementPresentHandler extends GenerateElementActionsHandl
 
         LOG.debug("Displaying member chooser dialog");
 
-        MemberChooser<ClassMember> chooser = new MemberChooser<>(dialogMembers, true, true, project, false, null);
+        MemberChooser<PsiElementClassMember<?>> chooser = new MemberChooser<>(dialogMembers, true, true, project, false, null);
         //noinspection DialogTitleCapitalization
         chooser.setTitle("generate isElementPresent()");
 
@@ -72,7 +83,7 @@ public class GenerateIsElementPresentHandler extends GenerateElementActionsHandl
             CommandProcessor.getInstance().executeCommand(project, () -> {
                 final int offset = editor.getCaretModel().getOffset();
                 try {
-                    doGenerate(project, editor, clazz, null);
+                    writeIsPresent(project, editor, clazz, chooser.getSelectedElements());
                 }
                 catch (GenerateCodeException e) {
                     final String message = e.getMessage();
@@ -156,5 +167,64 @@ public class GenerateIsElementPresentHandler extends GenerateElementActionsHandl
     @Override
     protected String getNothingAcceptedMessage() {
         return "No fields without isElementPresent() were found";
+    }
+
+    private static void writeIsPresent(final Project project, final Editor editor, final PsiClass clazz, List<PsiElementClassMember<?>> members) {
+        int offset = editor.getCaretModel().getOffset();
+        ArrayList<TemplateGenerationInfo> templates = new ArrayList<>();
+        templates.addAll(
+                WriteAction.compute(
+                                () -> DumbModeAccessType.RELIABLE_DATA_ONLY.ignoreDumbMode(
+                                        () -> GenerateMembersUtil.insertMembersAtOffset(clazz, offset, generateIsPresentPrototype(members))))
+                        .stream()
+                        .filter(member -> member instanceof TemplateGenerationInfo)
+                        .map(member -> (TemplateGenerationInfo) member).collect(Collectors.toList())
+        );
+        runTemplates(project, editor, templates, 0);
+    }
+
+
+    private static void runTemplates(final Project myProject, final Editor editor, final List<? extends TemplateGenerationInfo> templates, final int index) {
+        TemplateGenerationInfo info = templates.get(index);
+        final Template template = info.getTemplate();
+
+        PsiElement element = Objects.requireNonNull(info.getPsiMember());
+        final TextRange range = element.getTextRange();
+        WriteAction.run(() -> editor.getDocument().deleteString(range.getStartOffset(), range.getEndOffset()));
+        int offset = range.getStartOffset();
+        editor.getCaretModel().moveToOffset(offset);
+        editor.getScrollingModel().scrollToCaret(ScrollType.CENTER);
+        TemplateManager.getInstance(myProject).startTemplate(editor, template, new TemplateEditingAdapter() {
+            @Override
+            public void templateFinished(@NotNull Template template, boolean brokenOff) {
+                if (index + 1 < templates.size()) {
+                    ApplicationManager.getApplication().invokeLater(() -> WriteCommandAction.runWriteCommandAction(myProject, () ->
+                            runTemplates(myProject, editor, templates, index + 1)
+                    ));
+                }
+            }
+        });
+    }
+
+
+    private static List<PsiMethod> generateIsPresent(List<PsiElementClassMember<?>> members) {
+        List<PsiMethod> methods = new ArrayList<>();
+        for (PsiElementClassMember<?> member : members) {
+            PsiElement field = member.getPsiElement();
+            if (field instanceof PsiField) {
+                final Project project = field.getProject();
+                final PsiElementFactory factory = JavaPsiFacade.getElementFactory(project);
+                final PsiMethod isPresent = WebElementGrUtil.generateIsPresentPrototype((PsiField) field);
+                methods.add(isPresent);
+            }
+        }
+        return methods;
+    }
+
+    private static List<GenerationInfo> generateIsPresentPrototype(List<PsiElementClassMember<?>> members) {
+        List<PsiMethod> prototypes = generateIsPresent(members);
+        final List<GenerationInfo> methods = new ArrayList<>();
+        methods.addAll(prototypes.stream().map(prototype -> new PsiGenerationInfo(prototype)).collect(Collectors.toList()));
+        return methods;
     }
 }
